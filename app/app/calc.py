0ae4a222ad7e0921b6e52d30ac07013236317d15
Nithyamurthy <44640522+Nithyamurthy@users.noreply.github.com>
2022-12-03 13:57:10 +0530

    Fixed Linting by adding spaces bw commas and adding new line at eof

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,14 +2,13 @@
 Calculator functions
 """
 
-def add(x,y):
+def add(x , y):
     return x+y
 
-def subtract(x,y):
+def subtract(x , y):
     if x > y:
         return x-y
     elif x < y:
         return y-x
     else:
         return 0
-        
