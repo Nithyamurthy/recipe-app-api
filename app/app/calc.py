5594993bd45a7ef3592f86a55082b2f46347e144
Nithyamurthy <44640522+Nithyamurthy@users.noreply.github.com>
2022-12-03 14:07:02 +0530

    Fixed Linting 5

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,10 @@
 Calculator functions
 """
 
-def add(x,y):
+def add(x, y):
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     if x > y:
         return x - y
     elif x < y:
--- app/app/tests.py
+++ app/app/tests.py
@@ -8,9 +8,9 @@ from app import calc
 class CalcTests(SimpleTestCase):
     """Test the calc module"""
     def test_add_numbers(self):
-        res=calc.add(5,6)
-        self.assertEqual(res,11)
+        res=calc.add(5, 6)
+        self.assertEqual(res, 11)
     
     def test_subtract_numbers(self):
-        res=calc.subtract(10,15)
-        self.assertEqual(res,5)
+        res=calc.subtract(10, 15)
+        self.assertEqual(res, 5)
