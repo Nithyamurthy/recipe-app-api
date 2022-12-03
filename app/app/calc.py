50ad9d61624b41af289f2b53c1e7edfddb313bda
Nithyamurthy <44640522+Nithyamurthy@users.noreply.github.com>
2022-12-03 13:52:12 +0530

    Added Sample Tests

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    return x+y
+
+def subtract(x,y):
+    if x > y:
+        return x-y
+    elif x < y:
+        return y-x
+    else:
+        return 0
