7ff584f19d140ee88e91ca518338f7a9c4a2ff0c
Nithyamurthy <44640522+Nithyamurthy@users.noreply.github.com>
2022-12-03 13:59:48 +0530

    Added Sample Tests

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,12 +3,12 @@ Calculator functions
 """
 
 def add(x , y):
-    return x+y
+    return x + y
 
 def subtract(x , y):
     if x > y:
-        return x-y
+        return x - y
     elif x < y:
-        return y-x
+        return y - x
     else:
         return 0
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,8 +9,8 @@ class CalcTests(SimpleTestCase):
     """Test the calc module"""
     def test_add_numbers(self):
         res=calc.add(5 , 6)
-        self.assertEqual(res,11)
+        self.assertEqual(res , 11)
     
     def test_subtract_numbers(self):
         res=calc.subtract(10 , 15)
-        self.assertEqual(res,5)
+        self.assertEqual(res , 5)
