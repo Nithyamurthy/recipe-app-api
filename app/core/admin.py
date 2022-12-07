6ecff6a7ecfcc2271e6e3361aa61bf7399e1fd7e
Nithyamurthy <44640522+Nithyamurthy@users.noreply.github.com>
2022-12-07 14:18:07 +0530

    Setup Django admin using customizations

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,49 @@
+"""
+Django admin customization
+"""
 from django.contrib import admin # noqa
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email','name'] # To list all users
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        # The first parameter shows section in the admin page(None)
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',), # makes the page wider and tidier
+            'fields' : (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }), # use comma at the end since its a tuple
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+
