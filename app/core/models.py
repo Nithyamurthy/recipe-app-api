0e9f7a6b885129b09b2626582b7124666923acdc
Nithyamurthy <44640522+Nithyamurthy@users.noreply.github.com>
2022-12-07 13:44:32 +0530

    Created User models

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,42 @@
-from django.db import models # noqa
+"""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email')
+        user = self.model(email=self.normalize_email(email), **extra_fields) # calls User model
+        user.set_password(password)
+        user.save(using=self._db) # best practice when you create new instance
+
+        return user
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+
+    objects=UserManager()
+
+    USERNAME_FIELD='email'
+    # Defines the field used for authentication by default djangouser model
+    # We override that field to use email
